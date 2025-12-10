611a5b31ac6a8f3052962491ad2d9236c57cfe37
codebasedlearning <info@codebasedlearning.dev>
2025-12-11 00:44:02 +0100

    day 2 minor improvements

--- aoc/solutions/day02.cpp
+++ aoc/solutions/day02.cpp
@@ -31,27 +31,50 @@ struct IdPair {
     }
 };
 
-std::vector<IdPair> parseLines(std::ranges::input_range auto&& lines) {
-    std::vector<IdPair> result;
+std::vector<IdPair> to_idPairs(std::ranges::input_range auto&& lines) {
+    auto idPairs =
+        lines
+        | std::views::transform([](auto&& line) {
+              return std::views::split(line, ',')
+                   | std::views::transform([](auto&& chunk) { return string_view{chunk}; })
+                   | std::views::filter([](const string_view sv) { return !sv.empty(); });
+          })
+        | std::views::join
+        | std::views::transform(IdPair::of);
 
-    for (const auto &line : lines) {
-        for (auto chunk : std::views::split(line, ',')) {
-            if (auto sv = string_view{chunk}; !sv.empty())
-                result.emplace_back(IdPair::of(sv));
-        }
-    }
-    return result;
+    return std::ranges::to<std::vector<IdPair>>(idPairs);
+
+    // same as
+
+    // for (auto const &line : lines) {
+    //     auto chunks =
+    //         std::views::split(line, ',')
+    //         | std::views::transform([](auto&& rng) { return string_view{rng}; })
+    //         | std::views::filter([](const string_view sv){ return !sv.empty(); })
+    //         | std::views::transform(IdPair::of);
+    //
+    //     for (auto&& idp : chunks)
+    //         result.emplace_back(idp);
+    // }
+
+    // for (const auto &line : lines) {
+    //     for (auto&& chunk : std::views::split(line, ',')) {
+    //         if (auto sv = string_view{chunk}; !sv.empty())
+    //             result.emplace_back(IdPair::of(sv));
+    //     }
+    // }
 }
 
-using DivList = std::unordered_set<size_t>;
-const DivList& quotients(size_t n) {
-    static std::unordered_map<size_t, DivList> cache{ {1,DivList()} };
+using DivSet = std::unordered_set<size_t>;
+
+const DivSet& quotients(size_t n) {
+    static std::unordered_map<size_t, DivSet> cache{ {1,DivSet()} };
 
     if (n <= 1) n=1;
     if (const auto it = cache.find(n); it != cache.end())
         return it->second;
 
-    DivList result{1};
+    DivSet result{1};
     const auto limit = static_cast<size_t>(std::sqrt(static_cast<double>(n)));
     for (size_t d = 2; d <= limit; ++d) {
         if (n % d == 0) {
@@ -64,49 +87,38 @@ const DivList& quotients(size_t n) {
     return it->second;
 }
 
-bool checkInvalidIdPart1(const int64_t x) {
-    const auto s = aoc::to_string(x);
-    const size_t n = s.size();
+bool checkInvalidIdPart1(const string_view sv) {
+    const size_t n = sv.size();
     if (n % 2 != 0)
         return false;   // odd length cannot match
 
     const size_t h = n / 2;
-    const auto sv = string_view{s};
     return sv.substr(0, h) == sv.substr(h, h);
 }
 
-bool checkInvalidIdPart2(const int64_t x) {
-    const auto s = aoc::to_string(x);
-    const size_t n = s.size();
+bool checkInvalidIdPart2(const string_view sv) {
+    const size_t n = sv.size();
 
     return std::ranges::any_of(quotients(n), [&](size_t q) {
         return std::ranges::all_of(
             std::views::iota(q, n), // a sequence of numbers q..<n
-            [&](const size_t i) { return s[i] == s[i % q]; }
+            [&](const size_t i) { return sv[i] == sv[i % q]; }
         );
     });
-
-    /* this is basically:
-        for (const auto q : quotients(n)) {
-            bool matches = true;
-            for (size_t i = q; i < n && matches; ++i)
-                matches = (s[i] == s[i % q]);
-            if (matches) return true;
-        }
-        return false;
-    */
 }
 
 aoc::solutions solve(std::ranges::input_range auto&& lines) {
-    auto idPairs = parseLines(lines);
+    auto idPairs = to_idPairs(lines);
 
     int64_t sum1 = 0;
     int64_t sum2 = 0;
     for (const auto [id1, id2] : idPairs) {
         for (auto id = id1; id <= id2; ++id) {
-            if (checkInvalidIdPart1(id))
+            auto s = aoc::to_string(id);
+            const string_view sv{s};
+            if (checkInvalidIdPart1(sv))
                 sum1 += id;
-            if (checkInvalidIdPart2(id))
+            if (checkInvalidIdPart2(sv))
                 sum2 += id;
         }
     }
@@ -128,7 +140,7 @@ int main() {
     aoc::println(answer, ms);
 
     // 24157613387 (1227775554), 33832678380 (4174379265)
-    if constexpr (example==-1) { assert(answer.part1==24157613387 && answer.part2==33832678380); }
+    if constexpr (example==-1) { assert(answer.part1==24157613387 && answer.part2==33832678380); } // best 64 113ms
     if constexpr (example==0) { assert(answer.part1==1227775554 && answer.part2==4174379265); }
 
     return EXIT_SUCCESS;
